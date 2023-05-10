7ac74146278e18b94c7aff3da39181e415877ba9
Stephen Moss <bladexr8@gmail.com>
2023-05-10 20:15:44 +1000

    fix postgres ssl config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
             user: dbConfig.user,
             password: dbConfig.password,
             ssl: {
-                RejectUnauthorized: false
+                rejectUnauthorized: false
             }
         }
     }
