60078f685696b8e37493a25378b8de9227dbb074
Stephen Moss <bladexr8@gmail.com>
2023-05-10 20:38:40 +1000

    fix heroku config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-    url: env('API_HEROKU_URL')
+    url: env('API_HEROKU_URL'),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    }
 });
